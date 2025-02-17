798c68610f917f2f4e6b0156fe3e5b35b239f935
Liams-theCreator <mmeellaallii1@gmail.com>
2025-02-17 19:02:46 +0100

    setting up

---
+++ Makefile
@@ -0,0 +1,38 @@
+CC = cc
+FLAGS = -Werror -Wextra -Wall
+
+CLIENT_SRC = client.c utils.c
+CLIENT_OBJ = client.o utils.o
+CLIENT_X = client
+
+SERVER_SRC = server.c utils.c
+SERVER_OBJ = server.o utils.o
+SERVER_X = server
+
+all: $(CLIENT_X) $(SERVER_X)
+
+
+$(CLIENT_X): $(CLIENT_OBJ)
+	$(CC) $(FLAGS) -o $(CLIENT_X) $(CLIENT_OBJ)
+
+$(SERVER_X): $(SERVER_OBJ)
+	$(CC) $(FLAGS) -o $(SERVER_X) $(SERVER_OBJ)
+
+client.o: client.c
+	$(CC) $(FLAGS) -c client.c
+
+server.o: server.c
+	$(CC) $(FLAGS) -c server.c
+
+utils.o: utils.c
+	$(CC) $(FLAGS) -c utils.c
+
+clean:
+	rm -f $(CLIENT_OBJ) $(SERVER_OBJ)
+
+fclean: clean
+	rm -f $(CLIENT_X) $(SERVER_X)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imellali <imellali@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/17 18:28:33 by imellali          #+#    #+#             */
+/*   Updated: 2025/02/17 18:29:42 by imellali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+
+
+#endif
