d89442fd5a599a46460be0494fc2d476f6dd7a36
Liams-theCreator <mmeellaallii1@gmail.com>
2025-02-18 16:22:16 +0100

    conv

--- client.c
+++ client.c
@@ -6,8 +6,28 @@
 /*   By: imellali <imellali@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 18:27:44 by imellali          #+#    #+#             */
-/*   Updated: 2025/02/17 18:58:49 by imellali         ###   ########.fr       */
+/*   Updated: 2025/02/18 16:22:08 by imellali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+char	*conver_to_bin(char *msg)
+{
+	return (message);
+}
+int main(int argc, char **argv)
+{
+	char	*message;
+	int		pid;
+
+	if (argc < 3)
+		ft_exit();
+	pid = argv[2];
+	if (pid <= 2)
+		return ;
+	message = convert_to_bin(argv[1]);
+	if (!message)
+		return ;
+	return 0;
+}
--- server.c
+++ server.c
@@ -6,8 +6,19 @@
 /*   By: imellali <imellali@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 18:27:14 by imellali          #+#    #+#             */
-/*   Updated: 2025/02/17 18:59:05 by imellali         ###   ########.fr       */
+/*   Updated: 2025/02/18 15:34:45 by imellali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+int main(void)
+{
+	while (4)
+	{
+		printf("Server is running..\n");
+		printf("Server PID : %d\n", getpid());
+		pause();
+	}
+	return 0;
+}
